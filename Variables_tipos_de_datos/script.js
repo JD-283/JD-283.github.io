29936f4066f7a8fe02ca9c5f220dd11a50f31cc9
JD-283 <juandiegohl56@gmail.com>
2026-07-26 09:58:15 -0600

    Delete Variables_tipos_de_datos directory

--- Variables_tipos_de_datos/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btnEjecutar').addEventListener('click', function() {
-    // 1. Ejecutas tu lógica o función
-    let nombre = prompt("ingrese su nombre")
-    const resultado = ("¡Hola! " + nombre + " Este es el resultado del script");
-
-    // 2. Lo muestras dentro de la tarjeta
-    document.getElementById('pantalla-resultado').innerHTML = `<p>${resultado}</p>`;
-});
